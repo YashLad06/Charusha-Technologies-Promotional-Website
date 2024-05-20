26cd882264b9c9b2f38b5861d49b91d870860963
YashLad06 <yash.lad05@outlook.com>
2024-05-20 19:31:53 +0530

    added background transitions

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,18 @@
 // import { images } from '../../constants';
+import { useState } from 'react';
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 import './Header.css';
 
 const Header = () => {
+    const [selectedImage, setSelectedImage] = useState('image1');
+
+    const handleRadioChange = (event) => {
+        setSelectedImage(event.target.value);
+    };
+
     return (
-        <div className='app__header app__wrapper section__padding' id='home'>
+        // <div className='app__header app__wrapper section__padding header' id='home'>
+        <div className={` app__wrapper section__padding header ${selectedImage}`} id='home'>
             <div className='wrapper_info'>
 
                 <h1 className='app__header-h1'>
@@ -24,6 +32,14 @@ const Header = () => {
                     Our mission is to deliver exceptional digital solutions that drive success and create lasting value for our clients.
                 </p>
                 {/* <button type="button" className='custom__button'>Explore More</button> */}
+
+                <div className="radio-buttons">
+                    <input type="radio" id="image1" name="image" value="image1" checked={selectedImage === 'image1'} onChange={handleRadioChange} />
+                    <label htmlFor="image1"></label>
+
+                    <input type="radio" id="image2" name="image" value="image2" checked={selectedImage === 'image2'} onChange={handleRadioChange} />
+                    <label htmlFor="image2"></label>
+                </div>
             </div>
         </div>
     )
